311b3d326947f866afe795811126d3a0d8b56f85
CypriotUnknown <torosilgaz@gmail.com>
2025-03-10 14:19:39 +0200

    bumped up version

--- mod.ts
+++ mod.ts
@@ -1,6 +1,13 @@
-export * from "./src/encryptor.ts";
+import Encryptor from "./src/encryptor.ts";
+import ComputePostmanSecretDTO from "./src/models/models.computePostmanSecretDTO.ts";
+import ComputeSecretDTO from "./src/models/models.computeSecretDTO.ts";
+import EncryptedRequestBodyDTO from "./src/models/models.encryptedRequestBodyDTO.ts";
+import SecurityKeysOutput from "./src/models/models.securityKeysOutput.ts";
 
-export * from "./src/models/models.computePostmanSecretDTO.ts";
-export * from "./src/models/models.computeSecretDTO.ts";
-export * from "./src/models/models.encryptedRequestBodyDTO.ts";
-export * from "./src/models/models.securityKeysOutput.ts";
+export {
+    Encryptor,
+    ComputePostmanSecretDTO,
+    ComputeSecretDTO,
+    EncryptedRequestBodyDTO,
+    SecurityKeysOutput,
+}
